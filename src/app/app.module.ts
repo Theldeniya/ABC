5eaaab266853338a2b2fa5562957e09b028e0be9
Theldeniya <hashiniynet@gmail.com>
2018-09-09 00:34:44 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,66 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { RecipesComponent } from './component/recipes/recipes.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { PageComponent } from './component/PageNotfound/page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    RecipesComponent,
+    ContactComponent,
+    PageComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatGridListModule,
+    RouterModule
+    .forRoot([
+      {
+        path:'',
+        component:HomeComponent
+      },
+      {
+        path:'about',
+        component:AboutComponent
+      },
+      {
+        path:'contact',
+        component:ContactComponent
+      },
+      {
+        path:'recipes',
+        component:RecipesComponent
+      },
+      {
+        path:'pageNotfound',
+        component:PageComponent
+      },
+
+    ])
+
   ],
+
+  
+
   providers: [],
   bootstrap: [AppComponent]
 })
